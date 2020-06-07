a0a93a340c2dff4e3a8f3c6a939441e97ca57ca6
Donghyeon Kim <kevink3@naver.com>
2020-06-07 17:03:45 +0900

    map initial commit

--- AvoidCoronaApp/AvoidCoronaApp/AppDelegate.swift
+++ AvoidCoronaApp/AvoidCoronaApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAzSgc67S9wL4MrP5WmgY9YCpLzkU8UEyI")
+        GMSPlacesClient.provideAPIKey("AIzaSyAzSgc67S9wL4MrP5WmgY9YCpLzkU8UEyI")
         return true
     }
 
--- AvoidCoronaApp/AvoidCoronaApp/SecondViewController.swift
+++ AvoidCoronaApp/AvoidCoronaApp/SecondViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 
 class SecondViewController: UIViewController {
@@ -15,6 +16,20 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+               let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+               mapView.isMyLocationEnabled = true
+               view = mapView
+               
+               // Creates a marker in the center of the map.
+               let marker = GMSMarker()
+               marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+               marker.title = "Sydney"
+               marker.snippet = "Australia"
+               marker.map = mapView
     }
     
     
