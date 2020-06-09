6c061cd5f2c577d24fa7452746fa3485d1f6fe58
Shin <xenoshin777@naver.com>
2020-06-09 20:26:52 +0900

    COVID-19 국내 발생현황 표시

--- AvoidCoronaApp/AvoidCoronaApp/FirstViewController.swift
+++ AvoidCoronaApp/AvoidCoronaApp/FirstViewController.swift
@@ -14,8 +14,8 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var KoreaCOVIDTableView: UITableView!
     
-    var jsondata: [String: Any]!
-    var datacount = 0
+    var jsondata1: [String: Any]!
+    var jsondata2: [String: Any]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,23 +28,36 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
         titleLabel.numberOfLines = 0
         titleLabel.lineBreakMode = .byWordWrapping
         //COVID 데이터 JSON 파싱
-        getData {
-            let updateTime = (self.jsondata["updateTime"] as! String).components(separatedBy: "(")
+        getData1 {
+            let updateTime = (self.jsondata1["updateTime"] as! String).components(separatedBy: "(")
             self.titleLabel.text = updateTime[0]
             self.dateLabel.text = updateTime[1].components(separatedBy: ")")[0]
-            print("reload Data")
-            print(self.datacount)
+            self.KoreaCOVIDTableView.reloadData()
+        }
+        getData2 {
             self.KoreaCOVIDTableView.reloadData()
         }
     }
     
-    func getData(success: @escaping ()->()) {
+    func getData1(success: @escaping ()->()) {
         do {
             let url = URL(string: "http://api.corona-19.kr/korea")
             if let data = try String(contentsOf: url!).data(using: .utf8) {
-                jsondata = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                datacount = jsondata.count
-                print(jsondata)
+                jsondata1 = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(jsondata1)
+                success()
+            }
+        } catch let e as NSError {
+            print(e.localizedDescription)
+        }
+    }
+    
+    func getData2(success: @escaping ()->()) {
+        do {
+            let url = URL(string: "http://api.corona-19.kr/korea/country/new")
+            if let data = try String(contentsOf: url!).data(using: .utf8) {
+                jsondata2 = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(jsondata2)
                 success()
             }
         } catch let e as NSError {
@@ -53,7 +66,7 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 22
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -61,24 +74,62 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
         switch indexPath.row {
         case 0:
             cell.titleLabel.text = "누적 확진자 수"
-            cell.valueLabel.text = jsondata["TotalCase"] as? String
-            break
+            cell.valueLabel.text = jsondata1["TotalCase"] as? String
         case 1:
             cell.titleLabel.text = "누적 완치자 수"
-            cell.valueLabel.text = jsondata["TotalRecovered"] as? String
-            break
+            cell.valueLabel.text = jsondata1["TotalRecovered"] as? String
         case 2:
             cell.titleLabel.text = "누적 사망자 수"
-            cell.valueLabel.text = jsondata["TotalDeath"] as? String
-            break
+            cell.valueLabel.text = jsondata1["TotalDeath"] as? String
         case 3:
             cell.titleLabel.text = "격리 환자 수"
-            cell.valueLabel.text = jsondata["NowCase"] as? String
-            break
+            cell.valueLabel.text = jsondata1["NowCase"] as? String
+        case 4:
+            updateLabel(city_kor: "서울", city_eng: "seoul", cell: cell)
+        case 5:
+            updateLabel(city_kor: "부산", city_eng: "busan", cell: cell)
+        case 6:
+            updateLabel(city_kor: "대구", city_eng: "daegu", cell: cell)
+        case 7:
+            updateLabel(city_kor: "인천", city_eng: "incheon", cell: cell)
+        case 8:
+            updateLabel(city_kor: "광주", city_eng: "gwangju", cell: cell)
+        case 9:
+            updateLabel(city_kor: "대전", city_eng: "daejeon", cell: cell)
+        case 10:
+            updateLabel(city_kor: "울산", city_eng: "ulsan", cell: cell)
+        case 11:
+            updateLabel(city_kor: "세종", city_eng: "sejong", cell: cell)
+        case 12:
+            updateLabel(city_kor: "경기", city_eng: "gyeonggi", cell: cell)
+        case 13:
+            updateLabel(city_kor: "강원", city_eng: "gangwon", cell: cell)
+        case 14:
+            updateLabel(city_kor: "충북", city_eng: "chungbuk", cell: cell)
+        case 15:
+            updateLabel(city_kor: "충남", city_eng: "chungnam", cell: cell)
+        case 16:
+            updateLabel(city_kor: "전북", city_eng: "jeonbuk", cell: cell)
+        case 17:
+            updateLabel(city_kor: "전남", city_eng: "jeonnam", cell: cell)
+        case 18:
+            updateLabel(city_kor: "경북", city_eng: "gyeongbuk", cell: cell)
+        case 19:
+            updateLabel(city_kor: "경남", city_eng: "gyeongnam", cell: cell)
+        case 20:
+            updateLabel(city_kor: "제주", city_eng: "jeju", cell: cell)
+        case 21:
+            updateLabel(city_kor: "검역", city_eng: "quarantine", cell: cell)
         default:
             break
         }
         return cell
+    }
+    
+    func updateLabel(city_kor: String, city_eng: String, cell: COVIDCell) {
+        cell.titleLabel.text = city_kor
+        let data = jsondata2[city_eng] as! [String: Any]
+        cell.valueLabel.text = data["totalCase"] as? String
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
